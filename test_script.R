7394ff0a80bae15c8a974507daf32b388bf2799c
Amy Ehrhart <aehrhart@utah.gov>
2024-08-14 09:47:34 -0600

    Add ggplot of mean sepal length

--- test_script.R
+++ test_script.R
@@ -6,3 +6,8 @@ iris_grouped <- iris %>%
   group_by(Species) %>%
   summarize(mean_sepal_length = mean(Sepal.Length))
 iris_grouped
+
+ggplot(iris_grouped, aes(x=Species, y = mean_sepal_length)) +
+  geom_col() +
+  xlab("Species") +
+  ylab("Mean Sepal Length")
